8faf5dd78467a2d8ed5173cd69a7e2cfec37f38b
Adit Firdaus <64578017+Adit-Firdaus@users.noreply.github.com>
2023-10-18 08:14:49 +0700

    Socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,36 @@
+import http from 'http';
 import express, { Express } from 'express';
+import socketIo from 'socket.io';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-
-import router from './routes/api';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan('dev'));
+app.use(morgan('short'));
 
-app.use("/api", router);
+app.use(express.static('./src/public'));
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+const io = new socketIo.Server(server);
+
+io.on('connection', (socket: socketIo.Socket) => {
+  console.log(`A user connected ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected ${socket.id}`);
+  });
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
